88181dbede3970de941e78d408c7a5f9fadad401
Daniela-Mera <daniela.antonieta@gmail.com>
2022-05-06 11:07:39 -0400

    Version inicial con bootstrap

---
+++ app/controllers/pajarito_controller.rb
@@ -0,0 +1,27 @@
+class PajaritoController < ApplicationController
+
+    def index 
+        response = RestClient::Request.new(
+            :method => :get,
+            :url => "https://aves.ninjas.cl/api/birds"
+        ).execute
+
+        @resultado = JSON.parse(response)
+        @paginacion = Kaminari.paginate_array(@resultado).page(params[:page]).per(6)
+    end
+
+
+    def detalle 
+        @uid = params[:uid]
+        
+        RestClient.get("https://aves.ninjas.cl/api/birds/#{@uid}") { |response, request, result|
+            if response.code ==  200
+                @resultado = JSON.parse(response)
+            else
+                @resultado = "No hay resultados"
+            end
+        }
+
+    end
+
+end
